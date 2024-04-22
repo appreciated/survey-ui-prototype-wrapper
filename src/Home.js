3f6a97beb1170c7999578d6f6a65efa0b66f1355
Johannes Goebel <GoebelJohannes@gmx.net>
2024-04-22 23:53:32 +0200

    Styles

--- src/Home.js
+++ src/Home.js
@@ -17,7 +17,7 @@ const App = () => {
             setTimeLeft((prevTimeLeft) => {
                 if (prevTimeLeft <= 1) {
                     clearInterval(timer);
-                    return 0; // Stop the timer at 0
+                    return 0;
                 }
                 return prevTimeLeft - 1;
             });
@@ -30,15 +30,22 @@ const App = () => {
 
     return (
         <div className="app" style={{display: 'flex', flexDirection: 'column', width: '100%', height: '100%'}}>
-            <div style={{flex: "0 0 50px"}}><h1>{title}</h1></div>
-            <div style={{flex: "1 0 auto"}}>
-                <iframe src={iframeUrl} title="iframe" style={{display:(path ?"block":"none"), width: '100%', height: '100%', boxSizing: "border-box", border: "none"}}/>
-            </div>
-            <div style={{flex: "0 0 50px"}}>
-                <p>{instructions}</p>
-                <div>
+            <div className="header-bar" style={{display:"flex",flexDirection:"row",  flex: "0 0 50px"}}><h1>{title}</h1>
+                <div style={{display:"flex",flexDirection:"row"}}>
+                    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/>
+                    </svg>
                     <p>Timer: {Math.floor(timeLeft / 60)}:{('0' + (timeLeft % 60)).slice(-2)}</p>
                 </div>
+            </div>
+            <div style={{flex: "1 0 auto"}}>
+                <iframe src={iframeUrl} title="iframe" style={{
+                    display: (path ? "block" : "none"),
+                    width: '100%', height: '100%', boxSizing: "border-box", border: "none"
+                }}/>
+            </div>
+            <div className="footer-bar" style={{flex: "0 0 50px"}}>
+                <p>{instructions}</p>
             </div>
         </div>
     );
