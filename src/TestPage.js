e33ed23f72e8238ab126be67b7d2c4279ce694b6
Johannes Goebel <GoebelJohannes@gmx.net>
2024-04-23 01:00:29 +0200

    Styles

--- src/TestPage.js
+++ src/TestPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TestPage = () => {
     return (
-        <div>
+        <div style={{padding:'20px'}}>
             <h1>Test Page</h1>
             <p>This is a test page for demonstrating component functionality.</p>
         </div>
