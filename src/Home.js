542a0b41189b2a86a51d0c3159ff0964cb1a15b8
Johannes Goebel <GoebelJohannes@gmx.net>
2024-04-22 22:22:05 +0200

    Improve path query parameter handling

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ const App = () => {
         const params = new URLSearchParams(window.location.search);
         setTitle(params.get('title') || 'Standard Title');
         setInstructions(params.get('instructions') || 'Follow the instructions...');
-        setPath(params.get('path') || '/');
+        setPath(params.get('path'));
 
         // Set up timer
         const timer = setInterval(() => {
@@ -32,7 +32,7 @@ const App = () => {
         <div className="app" style={{display: 'flex', flexDirection: 'column', width: '100%', height: '100%'}}>
             <div style={{flex: "0 0 50px"}}><h1>{title}</h1></div>
             <div style={{flex: "1 0 auto"}}>
-                <iframe src={iframeUrl} title="iframe" style={{width: '100%', height: '100%', boxSizing: "border-box", border: "none"}}/>
+                <iframe src={iframeUrl} title="iframe" style={{display:(path ?"block":"none"), width: '100%', height: '100%', boxSizing: "border-box", border: "none"}}/>
             </div>
             <div style={{flex: "0 0 50px"}}>
                 <p>{instructions}</p>
